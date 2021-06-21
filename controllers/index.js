7056faf4e65bb9b8cc8a75c301833c0f993e76e3
Bobby T <bob.tarino@gmail.com>
2021-06-21 13:36:05 -0500

    added routes for dashboard, homepage, and an index at root of folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,17 @@
+// this index collects packaged group of API endpoints and prefixes them with path /api
 const router = require('express').Router();
 
+const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
-
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 // for endpoints that don't exist 
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
-  
+
 module.exports = router;
