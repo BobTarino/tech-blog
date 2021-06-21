2dcbec9fc5a1ec5a3bdffbe2d610b0a7cf6ef267
Bobby T <bob.tarino@gmail.com>
2021-06-21 15:04:42 -0500

    added frontend logic for posting and commenting

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,30 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    // get post id from url
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    // Get the post title and content from the form
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector('input[name="post-text"]').value;
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+
+}
+  
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
